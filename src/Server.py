3f18991e1179f41a99a245e4b9b7f2589dcc7861
giftedodd <giftedoddd@gmail.com>
2024-11-27 17:47:30 +0300

    updates on Server.py and .gitignore

--- src/Server.py
+++ src/Server.py
@@ -1,52 +1,43 @@
 import socket as sock
+import time
 from threading import Thread, Lock, Condition
 
 class Server:
-    def __init__(self, members, ip=None, port=9353):
-        self.ip = ip                            # Host's ip address: Local(Private) ip that host machine is currently running on.
-        self.port = port                        # Host's port: Integer value that need to be more than 1023 and less than 65535.
-        self.members = members                  # Number of participants of the game.
-        self.clients = {}                       # A dict to store connected clients.
-        self.lock = Lock()
-        self.condition = Condition(self.lock)
-        self.data = None
+    def __init__(self, members, port=9353):
+        self.__ip = None                                 # Host's ip address: Local(Private) ip that host machine is currently running on.
+        self.__data = None
+        self.__port = port                                # Host's port: Integer value that need to be more than 1023 and less than 65535.
+        self.__members = members                          # Number of participants of the game.
+        self.__clients = {}                               # A dict to store connected clients.
+        self.__lock = Lock()
+        self.__condition = Condition(self.__lock)
 
-    def __len__(self):
-        return len(self.clients)
+    def __len__(self) -> int:
+        return len(self.__clients)
 
-    def __repr__(self):
-        return f"Host Server Running At {self.ip}:{self.port}"
+    def __repr__(self) -> str:
+        return f"Host Server Running At {self.__ip}:{self.__port}"
 
-    def ip_check(self) -> None:
+    def __set_ip(self) -> None:
         """
         Checks if passed ip address is valid or not.
         args: None
         returns: None
         """
-        if self.ip is None:
-            try:
-                # Try a simple ping connection to 1.1.1.1 to get the private ip address
-                with sock.socket(sock.AF_INET, sock.SOCK_DGRAM) as connection:
-                    connection.settimeout(2)
-                    connection.connect(("1.1.1.1", 80))
-                    ip = connection.getsockname()
-                    if ip[0].startswith("127"):
-                        raise OSError
-                    # Assigning new valid ip address
-                    self.ip = ip[0]
-                    return
-
-            except OSError as e:
-                print(f"Function \"ip_check\" throws an Exception:\t{e}")
-            except Exception as e:
-                print(f"Crashed due unhandled Exception:\t{e}")
-
         try:
-            with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as socket:
-                socket.bind((self.ip, self.port))
-        except OSError:
-            self.ip = None
-            self.ip_check()
+            # Try a simple ping connection to 1.1.1.1 to get the private ip address
+            with sock.socket(sock.AF_INET, sock.SOCK_DGRAM) as connection:
+                connection.settimeout(2)
+                connection.connect(("1.1.1.1", 80))
+                ip = connection.getsockname()
+                if ip[0].startswith("127"):
+                    raise OSError
+                # Assigning new valid ip address
+                self.__ip = ip[0]
+        except OSError as e:
+            print(f"Function \"ip_check\" throws an Exception:\t{e}")
+        except Exception as e:
+            print(f"Crashed due unhandled Exception:\t{e}")
 
     def start_server(self):
         """
@@ -80,17 +71,24 @@ class Server:
             print(f"Connection established with {sock.getfqdn(client_address[0])} from {client_address[0]}")
             while True:
                 data = client_socket.recv(1024).decode(encoding="utf-8")
-                if not self.data:
+                if not data:
                     print(f"Connection Closed by client{client_address}")
-                    break
+                    time.sleep(0.5)
+                    continue
                 with self.lock:
                     self.data = data
                     self.condition.notify_all()
     # TODO:NOT FINISHED YET
     def get_command(self):
         with self.condition:
-            while self.condition is None:
+            while self.data is None:
                 self.condition.wait()
             data = self.data
             self.data = None
         return data
+
+s = Server(1, ip="192.168.146.158")
+s.start_server()
+while True:
+    mamad = s.get_command()
+    print(mamad)
